18bcd4461fd75cdb63a3ef78bf8e0a317224cf99
Ahmed Shokr <93308652+ahmedShokrr@users.noreply.github.com>
2025-05-05 23:55:17 +0300

    Update 13. Roman to Integer.java

--- Easy/13. Roman to Integer.java
+++ Easy/13. Roman to Integer.java
@@ -2,39 +2,28 @@ import java.util.HashMap;
 
 class Solution {
     public static int romanToInt(String s) {
-        int total=0;
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
 
-        HashMap<String,Integer> romanMap =new HashMap<>();
-        romanMap .put("I",1);
-        romanMap .put("V",5);
-        romanMap .put("X",10);
-        romanMap .put("L",50);
-        romanMap .put("C",100);
-        romanMap .put("D",500);
-        romanMap .put("M",1000);
+        int total = 0;
+        int prevValue = 0;
 
-        int inputLen=s.length();
-        for (int i = inputLen - 1; i >= 0; i--) {
-            if (i!=0){
-                char currentChar = s.charAt(i);
-                char previousChar = s.charAt(i - 1);
-                int currentValue = romanMap.get(String.valueOf(currentChar));
-                int previousValue = romanMap.get(String.valueOf(previousChar));
-
-                if (currentValue <=previousValue){
-                    total=total+currentValue;
-                }else{
-                    total=total+currentValue;
-                    total=total-previousValue;
-                    i--;  // Skip the previous character since it's been processed
-                }
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = romanMap.get(s.charAt(i));
+            if (currentValue < prevValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
             }
-            else {
-                char last = s.charAt(i);
-                int lastValue = romanMap.get(String.valueOf(last));
-                total = total + lastValue;
-            }
+            prevValue = currentValue;
         }
+
         return total;
     }
 
